ba1b6cc6b7d681dc2b802529eaf12a29168b9e0e
OmnipotentBanana <juanmyburg@gmail.com>
2022-04-14 12:59:50 +0200

    Edited Message.java and User.java

--- src/main/java/com/npg005/message/Message.java
+++ src/main/java/com/npg005/message/Message.java
@@ -1,4 +1,27 @@
 package com.npg005.message;
+import java.time.LocalDateTime;
 
 public class Message {
+    private String message;
+    private LocalDateTime timeSent;
+
+    public Message(String message, LocalDateTime timeSent) {
+        this.message = message;
+        this.timeSent = timeSent;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getTimeSent() {
+        return timeSent;
+    }
+
+    public void setTimeSent(LocalDateTime timeSent) {
+        this.timeSent = timeSent;
+    }
 }
--- src/main/java/com/npg005/user/user.java
+++
@@ -1,6 +0,0 @@
-package com.npg005.user;
-
-public class user {
-    private String username;
-    private String message;
-}
