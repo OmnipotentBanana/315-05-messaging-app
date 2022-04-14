06736c3685d90d0c45824e83eaab679deedabaa9
OmnipotentBanana <juanmyburg@gmail.com>
2022-04-14 13:03:23 +0200

    Discord Webhook Test

--- src/main/java/com/npg005/user/User.java
+++ src/main/java/com/npg005/user/User.java
@@ -7,6 +7,7 @@ public class User {
     public User(String username, String message) {
         this.username = username;
         this.message = message;
+        System.out.println("This is a test");
     }
 
     public String getUsername() {
